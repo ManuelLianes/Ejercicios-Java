659fb5e007abb3b42df50bed2a29e97e3d262254
Manuel Lianes <52200679+ManuelLianes@users.noreply.github.com>
2019-06-30 18:15:18 +0200

    Add files via upload

---
+++ Capitulo3/Fibonacci.java
@@ -0,0 +1,25 @@
+package Capitulo3;
+
+import java.util.Scanner;
+
+/**
+ * Fibonacci
+ */
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+        long n1, n2, suma;
+        System.out.print("Introduce iteraciones de la sucesion: ");
+        int iterador = consola.nextInt();
+
+        n1 = 0;
+        n2 = 1;
+        for (int i = 0; i < iterador; i++) {
+            suma = n1 + n2;
+            System.out.println(n1 + " + " + n2 + " = " + suma);
+            n1 = n2;
+            n2 = suma;
+        }
+    }
+}
